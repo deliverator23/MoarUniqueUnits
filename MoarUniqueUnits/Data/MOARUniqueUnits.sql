61e7ea4e52e84e8fe3914c3df24da400b107d96b
iainm <scarab123>
2018-04-13 14:03:27 +0100

    Remove Hunter promotion.

--- MoarUniqueUnits/Data/MOARUniqueUnits.sql
+++ MoarUniqueUnits/Data/MOARUniqueUnits.sql
@@ -75,42 +75,6 @@ UPDATE EnabledUniqueUnits SET Enabled = 0 WHERE NOT EXISTS (SELECT 1 FROM Civili
 
 
 
-
--- Hunter Promotion - does this work with Unit Upgrades?
-
-INSERT INTO Types (Type, Kind) VALUES ('PROMOTION_CLASS_HUNTER','KIND_PROMOTION_CLASS');
-
-INSERT INTO Types (Type, Kind)
-		SELECT UnitPromotionType || '_HUNTER','KIND_PROMOTION'
-		FROM UnitPromotions
-		WHERE PromotionClass = 'PROMOTION_CLASS_RANGED';
-
-INSERT INTO UnitPromotionClasses (PromotionClassType, Name)
-VALUES ('PROMOTION_CLASS_HUNTER', 'Hunter');
-
-INSERT INTO UnitPromotions(UnitPromotionType, Name, Description, Level, Column, PromotionClass)
-		SELECT UnitPromotionType || '_HUNTER', Name, Description, Level, Column, 'PROMOTION_CLASS_HUNTER'
-FROM UnitPromotions
-WHERE PromotionClass = 'PROMOTION_CLASS_RANGED';
-
-INSERT INTO UnitPromotionPrereqs(UnitPromotion, PrereqUnitPromotion)
-		SELECT UnitPromotion || '_HUNTER',  PrereqUnitPromotion || '_HUNTER'
-		FROM UnitPromotionPrereqs
-		WHERE UnitPromotion IN (SELECT UnitPromotionType FROM UnitPromotions WHERE PromotionClass =  'PROMOTION_CLASS_RANGED')
-					OR PrereqUnitPromotion IN (SELECT UnitPromotionType FROM UnitPromotions WHERE PromotionClass =  'PROMOTION_CLASS_RANGED');
-
-INSERT INTO UnitPromotionModifiers(UnitPromotionType, ModifierId)
-		SELECT UnitPromotionType || '_HUNTER',  ModifierId
-		FROM   UnitPromotionModifiers
-		WHERE UnitPromotionType IN (SELECT UnitPromotionType FROM UnitPromotions WHERE PromotionClass =  'PROMOTION_CLASS_RANGED');
-
-UPDATE UnitPromotions SET Name = 'LOC_PROMOTION_HUNTER_NAME', Description = 'LOC_PROMOTION_HUNTER_DESCRIPTION' WHERE UnitPromotionType = 'PROMOTION_VOLLEY_HUNTER';
-
-
-INSERT INTO UnitPromotionModifiers (UnitPromotionType, ModifierId) VALUES ('PROMOTION_VOLLEY_HUNTER', 'PLUS_5_COMBAT_STRENGTH_NON_DISTRICT');
-
-
-
 -- CONDITIONALS (Unit Stuff)
 
 --Types (KIND_UNIT)
